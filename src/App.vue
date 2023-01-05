164194bc1215522229726c19ab076b591428bfcc
victoriacassolo <vitoria.cassolo@gmail.com>
2023-01-05 11:25:48 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import Nav from './components/Nav.vue';
+</script>
+<template>
+  <div>
+    <Nav />
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><h1>About Vue</h1></div>
+</template>
